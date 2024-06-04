cfcda7653b1becb3db9750eb8527bd92c6ca34e9
Nanda <anandalukman01@gmail.com>
2024-06-04 18:16:50 +0700

    perbaikan

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
       </Head>
       <main className="bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
         <section className="min-h-screen">
-          <nav className="py-10 mb-12 flex justify-between dark:text-white">
+          <nav className="py-10 mb-12 flex justify-between dark:text-white text-black">
             <h1 className="font-burtons text-xl">Ananda Lukman Ramadhan</h1>
             <ul className="flex items-center">
               <li>
@@ -43,7 +43,7 @@ export default function Home() {
             <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
               Ananda Lukman Ramadhan
             </h2>
-            <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
+            <h3 className="text-2xl py-2 text-black dark:text-white md:text-3xl">
               Indie Developer.
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
@@ -53,14 +53,21 @@ export default function Home() {
               <AiFillLinkedin />
               <AiFillYoutube />
             </div>
-            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
-              <img src={"gambar.jpg"} layout="fill" objectFit="cover" alt="Nanda Kaws" />
+            <div className="justify-center items-center flex">
+              <Image
+                className="rounded-full mt-10"
+                src="/gambar.jpg"
+                width={320}
+                height={320}
+                objectFit="cover"
+                alt="Nanda Kaws"
+              />
             </div>
           </div>
         </section>
         <section>
           <div>
-            <h3 className="text-3xl py-1 dark:text-white">About me🔥</h3>
+            <h3 className="text-3xl py-1 text-black dark:text-white">About me🔥</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               Perkenalkan nama saya Ananda Lukman Ramadhan, kamu boleh memanggilku Nanda. Saya seorang Indie Developer. Saya berpengalaman magang selama 3 bulan sebagai IT support di RSUD Cengkareng. Di waktu luang, saya belajar dasar-dasar pemrograman di sana dan perjalanan programming saya dimulai sejak saya berusia 17 tahun. Awalnya saya memulai dari frontend dengan React Js dan hingga saat ini saya sudah mencapai Backend dengan Express Js.
             </p>
@@ -68,52 +75,46 @@ export default function Home() {
         </section>
         <section className="py-10">
           <div>
-            <h3 className="text-3xl py-1 dark:text-white">Portofolio</h3>
+            <h3 className="text-3xl py-1 text-black dark:text-white">Portofolio</h3>
           </div>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
             <div className="basis-1/3 flex-1">
-              <h1 className="text-2xl font-bold text-white text-center mb-4">Al Quran Website</h1>
+              <h1 className="text-2xl font-bold text-black dark:text-white text-center mb-4">Al Quran Website</h1>
               <Link href="https://readalquran.vercel.app/">
-                <a>
-                  <img
-                    className="rounded-lg object-cover hover:scale-105 duration-200 cursor-pointer"
-                    width={"100%"}
-                    height={"100%"}
-                    layout="responsive"
-                    src={"alquran.png"}
-                    alt="Al Quran"
-                  />
-                </a>
+                <Image
+                  className="rounded-lg object-cover hover:scale-105 duration-200 cursor-pointer"
+                  width={1000}
+                  height={1000}
+                  layout="responsive"
+                  src="/alquran.png"
+                  alt="Al Quran"
+                />
               </Link>
             </div>
             <div className="basis-1/3 flex-1">
-              <h1 className="text-2xl font-bold text-white text-center mb-4">API Secure Library</h1>
+              <h1 className="text-2xl font-bold text-black dark:text-white text-center mb-4">API Secure Library</h1>
               <Link href="https://www.npmjs.com/package/headerlock">
-                <a>
-                  <img
-                    className="rounded-lg object-cover hover:scale-105 duration-200 cursor-pointer"
-                    width={"100%"}
-                    height={"100%"}
-                    layout="responsive"
-                    src={"header.png"}
-                    alt="HeaderLock"
-                  />
-                </a>
+                <Image
+                  className="rounded-lg object-cover hover:scale-105 duration-200 cursor-pointer"
+                  width={1000}
+                  height={1000}
+                  layout="responsive"
+                  src="/header.png"
+                  alt="HeaderLock"
+                />
               </Link>
             </div>
             <div className="basis-1/3 flex-1">
-              <h1 className="text-2xl font-bold text-white text-center mb-4">VS Code Extension Started Template</h1>
+              <h1 className="text-2xl font-bold text-black dark:text-white text-center mb-4">VS Code Extension Started Template</h1>
               <Link href="https://marketplace.visualstudio.com/items?itemName=NandaKaws.bociljs">
-                <a>
-                  <img
-                    className="rounded-lg object-cover hover:scale-105 duration-200 cursor-pointer"
-                    width={"100%"}
-                    height={"60%"}
-                    layout="responsive"
-                    src={"extension.png"}
-                    alt="Vscode"
-                  />
-                </a>
+                <Image
+                  className="rounded-lg object-cover hover:scale-105 duration-200 cursor-pointer"
+                  width={1000}
+                  height={600}
+                  layout="responsive"
+                  src="/extension.png"
+                  alt="Vscode"
+                />
               </Link>
             </div>
           </div>
